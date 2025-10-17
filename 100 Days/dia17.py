80f4dc11b6f2372813fa11c5876cafe851259310
its-diegollull <dallull@miuandes.cl>
2025-10-17 16:40:00 -0300

    dia17

---
+++ 100 Days/dia17.py
@@ -0,0 +1,13 @@
+#Pide al usuario un número entero y suma todos sus dígitos.
+
+numero = int(input("Dame un numero: "))
+numero2 = str(numero)
+suma = 0
+for digito in numero2:
+    int(digito)
+    suma += int(digito)
+print(suma)
+
+    
+
+
